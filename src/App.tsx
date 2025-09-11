2b088767db83dc9e0802e3f111e0333874dcfe99
unknown <you@example.com>
2025-09-11 16:09:46 -0500

    Mi primer commit: subiendo el proyecto de React y Vite

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import {useState} from 'react'
+
+function App(){
+    const [list, setList] = useState string();
+    const []
+return(
+    <div className="App">
+        <input/>
+      <button>+</button>
+      <hr />
+      <ul>
+        <li>
+            <input type="checkbox" />
+            <span>texto</span>
+            <button>x</button>
+        </li>
+      </ul>
+    </div>
+)
+}
+
+export default App
+
